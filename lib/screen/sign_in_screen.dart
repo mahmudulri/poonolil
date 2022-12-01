ba686ee7c56ea588cf232637b353108458755bb8
MD. Mahmudul Hasan <mahmudul.ri@gmail.com>
2022-12-01 12:36:58 +0600

    fix problem homepage

--- lib/screen/sign_in_screen.dart
+++ lib/screen/sign_in_screen.dart
@@ -11,144 +11,179 @@ class SignInScreen extends StatelessWidget {
     var screenWidth = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         body: Container(
           height: screenHeight,
           width: screenWidth,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/images/homeback.png"),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: screenHeight * 0.100,
-                ),
-                Image.asset("assets/logo/poonolil_logo.png"),
-                SizedBox(
-                  height: screenHeight * 0.450,
-                ),
-                Container(
-                  width: screenWidth * 0.800,
-                  height: screenHeight * 0.080,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    color: Color(0xff7C6768).withOpacity(0.7),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      left: 20,
-                      right: 20,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                            flex: 5,
-                            child: TextField(
-                              decoration: InputDecoration(
-                                hintText: "Phone number".toUpperCase(),
-                                hintStyle: TextStyle(
-                                  fontFamily: "MADE Outer Sans Thin",
-                                  fontSize: 20,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w300,
-                                ),
-                                border: InputBorder.none,
+          // decoration: BoxDecoration(
+          //   image: DecorationImage(
+          //     image: AssetImage("assets/images/homeback.png"),
+          //     fit: BoxFit.cover,
+          //   ),
+          // ),
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Image.asset(
+                "assets/images/homeback.png",
+                fit: BoxFit.cover,
+              ),
+              LayoutBuilder(
+                builder:
+                    (BuildContext context, BoxConstraints viewportConstraints) {
+                  return SingleChildScrollView(
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(
+                        minHeight: viewportConstraints.maxHeight,
+                      ),
+                      child: IntrinsicHeight(
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: screenHeight * 0.100,
                               ),
-                            )),
-                        Expanded(
-                          flex: 1,
-                          child: Icon(
-                            Icons.phone_android,
-                            color: Color(0xffFEF9FA),
-                            size: 35,
+                              Image.asset("assets/logo/poonolil_logo.png"),
+                              SizedBox(
+                                height: screenHeight * 0.300,
+                              ),
+                              Container(
+                                width: screenWidth * 0.800,
+                                height: screenHeight * 0.080,
+                                decoration: BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10)),
+                                  color: Color(0xff7C6768).withOpacity(0.7),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                    left: 20,
+                                    right: 20,
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(
+                                          flex: 5,
+                                          child: TextField(
+                                            decoration: InputDecoration(
+                                              hintText:
+                                                  "Phone number".toUpperCase(),
+                                              hintStyle: TextStyle(
+                                                fontFamily:
+                                                    "MADE Outer Sans Thin",
+                                                fontSize: 20,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w300,
+                                              ),
+                                              border: InputBorder.none,
+                                            ),
+                                          )),
+                                      Expanded(
+                                        flex: 1,
+                                        child: Icon(
+                                          Icons.phone_android,
+                                          color: Color(0xffFEF9FA),
+                                          size: 35,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: screenHeight * 0.020,
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: ((context) => OtpScreen())));
+                                },
+                                child: Container(
+                                  width: screenWidth * 0.800,
+                                  height: screenHeight * 0.080,
+                                  decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10)),
+                                    color: Color(0xff96351E).withOpacity(0.7),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                      left: 20,
+                                      right: 20,
+                                    ),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          "Continue".toUpperCase(),
+                                          style: TextStyle(
+                                            fontFamily: "MADE Outer Sans Thin",
+                                            fontSize: 20,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w300,
+                                          ),
+                                        ),
+                                        SvgPicture.asset(
+                                          "assets/icons/arrow-down-right.svg",
+                                          height: 40,
+                                          width: 30,
+                                          color: Color(0xffDBB98F),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: screenHeight * 0.020,
+                              ),
+                              Text(
+                                "Or sign in with".toUpperCase(),
+                                style: TextStyle(
+                                  fontFamily: "MADE Outer Sans Thin",
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w200,
+                                ),
+                              ),
+                              SizedBox(
+                                height: screenHeight * 0.010,
+                              ),
+                              Container(
+                                width: screenWidth * 0.800,
+                                height: screenHeight * 0.080,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SvgPicture.asset(
+                                        "assets/icons/apple-button.svg"),
+                                    SizedBox(
+                                      width: screenWidth * 0.030,
+                                    ),
+                                    SvgPicture.asset(
+                                        "assets/icons/google-button.svg"),
+                                    SizedBox(
+                                      width: screenWidth * 0.030,
+                                    ),
+                                    SvgPicture.asset(
+                                        "assets/icons/facebook-button.svg"),
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: screenHeight * 0.020,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: ((context) => OtpScreen())));
-                  },
-                  child: Container(
-                    width: screenWidth * 0.800,
-                    height: screenHeight * 0.080,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      color: Color(0xff96351E).withOpacity(0.7),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        left: 20,
-                        right: 20,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Continue".toUpperCase(),
-                            style: TextStyle(
-                              fontFamily: "MADE Outer Sans Thin",
-                              fontSize: 20,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w300,
-                            ),
-                          ),
-                          SvgPicture.asset(
-                            "assets/icons/arrow-down-right.svg",
-                            height: 40,
-                            width: 30,
-                            color: Color(0xffDBB98F),
-                          ),
-                        ],
                       ),
                     ),
-                  ),
-                ),
-                SizedBox(
-                  height: screenHeight * 0.020,
-                ),
-                Text(
-                  "Or sign in with".toUpperCase(),
-                  style: TextStyle(
-                    fontFamily: "MADE Outer Sans Thin",
-                    fontSize: 16,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w200,
-                  ),
-                ),
-                SizedBox(
-                  height: screenHeight * 0.010,
-                ),
-                Container(
-                  width: screenWidth * 0.800,
-                  height: screenHeight * 0.080,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset("assets/icons/apple-button.svg"),
-                      SizedBox(
-                        width: screenWidth * 0.030,
-                      ),
-                      SvgPicture.asset("assets/icons/google-button.svg"),
-                      SizedBox(
-                        width: screenWidth * 0.030,
-                      ),
-                      SvgPicture.asset("assets/icons/facebook-button.svg"),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+                  );
+                },
+              )
+            ],
           ),
         ),
       ),
